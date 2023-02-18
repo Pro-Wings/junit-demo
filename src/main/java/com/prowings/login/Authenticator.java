7d1da7cae30ccbbdb1204963ad6d146c34ac6c5a
JD <jayprowings@gmail.com>
2023-02-18 09:03:46 +0530

    first draft

---
+++ src/main/java/com/prowings/login/Authenticator.java
@@ -0,0 +1,21 @@
+package com.prowings.login;
+
+public class Authenticator {
+	
+	public boolean login(String username, String password)
+	{
+		
+		if(username.equalsIgnoreCase("abc1234") && password.equalsIgnoreCase("P@ssword"))
+		{
+			return true;
+		}
+		else
+			return false;
+	}
+
+	public void timeout() throws InterruptedException
+	{
+		Thread.sleep(900);
+	}
+	
+}
